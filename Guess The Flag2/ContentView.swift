815f5f8136c497d4f0a4b4371988cf5dc3651d0f
Jahvon Capel <jahvoncoding@gmail.com>
2022-04-06 16:00:40 -0400

    Initial Commit

---
+++ Guess The Flag2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Guess The Flag2
+//
+//  Created by Jahvon on 4/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Guess The Flag2/Guess_The_Flag2App.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_The_Flag2App.swift
+//  Guess The Flag2
+//
+//  Created by Jahvon on 4/6/22.
+//
+
+import SwiftUI
+
+@main
+struct Guess_The_Flag2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
